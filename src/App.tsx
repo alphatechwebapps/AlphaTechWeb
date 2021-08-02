14c17eed4ac25152726184d28f37a2ffe528f60d
atmarcosrodrigues <antonio.rodrigues.ct@gmail.com>
2021-08-02 14:41:21 -0300

    Alphatech React v1

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Alpha Technology</h1>
+        <img src={logo} className="logo" alt="logo" />
+        <p>
+          Development of systems, sites and applications web/mobile.
+        </p>
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
